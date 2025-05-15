61bdc70143ae0cd76b9755851eaa644e08346cf3
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2025-05-15 23:27:12 +0200

    chore: simplify linting setup by removing `stylelint` and `markdownlint`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,57 +1,45 @@
-import eslintPluginJs from "@eslint/js";
-import eslintPluginStylistic from "@stylistic/eslint-plugin";
+import css from "@eslint/css";
+import {defineConfig} from "eslint/config";
 import globals from "globals";
-import {flatConfigs as importConfigs} from "eslint-plugin-import-x";
+import {flatConfigs as importX} from "eslint-plugin-import-x";
+import js from "@eslint/js";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import stylistic from "@stylistic/eslint-plugin";
 
-const config = [
-  eslintPluginJs.configs.all,
-  eslintPluginStylistic.configs.all,
-  importConfigs.recommended,
+export default defineConfig([
+  {files: ["**/*.css"], languageOptions: {tolerant: true}, plugins: {css}, language: "css/css", extends: ["css/recommended"], rules: {"css/use-baseline": ["error", {available: "newly"}]}},
   {
-    "files": ["**/*.js"],
-    "languageOptions": {
-      "globals": {
+    files: ["**/*.js", "**/*.mjs"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: {
         ...globals.browser,
         ...globals.node
-      },
-      "sourceType": "commonjs"
+      }
     },
-    "rules": {
+    plugins: {js, stylistic},
+    extends: [importX.recommended, "js/all", "stylistic/all"],
+    rules: {
       "@stylistic/array-element-newline": ["error", "consistent"],
       "@stylistic/dot-location": ["error", "property"],
       "@stylistic/function-call-argument-newline": ["error", "consistent"],
       "@stylistic/indent": ["error", 2],
       "@stylistic/multiline-comment-style": "off",
+      "@stylistic/object-property-newline": ["error", {allowAllPropertiesOnSameLine: true}],
       "@stylistic/padded-blocks": ["error", "never"],
       "@stylistic/quote-props": ["error", "as-needed"],
       "capitalized-comments": "off",
-      "line-comment-position": "off",
+      "import-x/no-unresolved": ["error", {ignore: ["eslint/config", "node_helper", "logger"]}],
       "max-lines-per-function": ["error", 100],
       "max-statements": ["error", 25],
-      "multiline-comment-style": "off",
-      "no-await-in-loop": "off",
       "no-inline-comments": "off",
       "no-magic-numbers": "off",
-      "one-var": "off",
+      "one-var": ["error", "never"],
       "sort-keys": "off",
-      "strict": "off"
+      strict: "off"
     }
   },
-  {
-    "files": ["**/*.mjs"],
-    "languageOptions": {
-      "ecmaVersion": "latest",
-      "globals": {
-        ...globals.node
-      },
-      "sourceType": "module"
-    },
-    "rules": {
-      "@stylistic/array-element-newline": ["error", "consistent"],
-      "@stylistic/indent": ["error", 2],
-      "no-magic-numbers": ["error", {"ignore": [2, 25, 100]}]
-    }
-  }
-];
-
-export default config;
+  {files: ["**/*.json"], ignores: ["package-lock.json"], plugins: {json}, extends: ["json/recommended"], language: "json/json"},
+  {files: ["**/*.md"], plugins: {markdown}, extends: ["markdown/recommended"], language: "markdown/gfm"}
+]);
